72665d52436f26b85e2b13863195e57a131656da
HaidyElkenawy <160390133+HaidyElkenawy@users.noreply.github.com>
2025-12-08 14:34:32 +0200

    deployment test

--- backend/server.js
+++ backend/server.js
@@ -18,4 +18,8 @@ app.use("/api/purchases", purchaseRoutes);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Books Assignment Backend is running");
+});
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
